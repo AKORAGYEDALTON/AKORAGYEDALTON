3c5e559cf359a6d2456db4472c8678ea246204ed
AKORAGYEDALTON <daltonakoragye20@gmail.com>
2024-10-26 13:10:47 +0300

    added a new code

---
+++ my course in django.py
@@ -0,0 +1,18 @@
+print("dalton ak")
+print('0----')
+print(' ||||')
+print('*' * 10)
+price = 10
+price = 20
+rating = 4.9
+name = 'john'
+age = 20
+is_new = True
+print(name, age ,is_new)
+name = input('what is your name? ')
+print('hi' + name)
+name = input('whats your favourite colour?' )
+print(name + ' likes' )
+birth_year =input('birth year: ')
+age = 2019 -int(birth_year)
+print(age)
